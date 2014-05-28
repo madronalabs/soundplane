5f615fe031c04d99a7c6a47238373c38fbc2dc0d
Randy Jones <randy@madronalabs.com>
2014-05-28 11:07:41 -0700

    added a test to draw normal map with better visualization.

--- Source/SoundplaneController.cpp
+++ Source/SoundplaneController.cpp
@@ -194,7 +194,8 @@ void SoundplaneController::setupMenus()
 	viewMenu->addItem("calibrated");
 	viewMenu->addItem("cooked");
 	viewMenu->addItem("xy");
-	viewMenu->addItem("test");
+	viewMenu->addItem("test1");
+	viewMenu->addItem("test2");
 	viewMenu->addItem("norm. map");
 	
 	mMenuMap["midi_device"] = MLMenuPtr(new MLMenu("midi_device"));
