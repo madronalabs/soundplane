17b96a0657b280180021dbf6736cc27ae33080d2
Randy Jones <randy@madronalabs.com>
2015-03-12 09:14:09 -0700

    rename binary files for new JSON options

--- Data/SoundplaneBinaryData/SoundplaneBinaryData.h
+++ Data/SoundplaneBinaryData/SoundplaneBinaryData.h
@@ -17,17 +17,17 @@ namespace SoundplaneBinaryData
     extern const char*  arrowrightdown_svg;
     const int           arrowrightdown_svgSize = 684;
 
-    extern const char*  chromaticfourths_json;
-    const int           chromaticfourths_jsonSize = 488;
-
-    extern const char*  chromaticoctaves_json;
-    const int           chromaticoctaves_jsonSize = 483;
-
-    extern const char*  continuous_json;
-    const int           continuous_jsonSize = 100;
+    extern const char*  chromatic_json;
+    const int           chromatic_jsonSize = 100;
 
     extern const char*  dot64_png;
     const int           dot64_pngSize = 10106;
+
+    extern const char*  rows_in_fourths_json;
+    const int           rows_in_fourths_jsonSize = 488;
+
+    extern const char*  rows_in_octaves_json;
+    const int           rows_in_octaves_jsonSize = 483;
 
 };
 
