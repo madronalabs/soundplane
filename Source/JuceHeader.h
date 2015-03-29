df1102ecf570f691874d19f7acdf821db4716ae9
Randy Jones <randy@madronalabs.com>
2015-03-29 11:33:25 -0700

    new headers to fix GL 2 / GL3 confusion

--- Source/JuceHeader.h
+++ Source/JuceHeader.h
@@ -18,6 +18,7 @@
 #include "modules/juce_graphics/juce_graphics.h"
 #include "modules/juce_gui_basics/juce_gui_basics.h"
 #include "modules/juce_gui_extra/juce_gui_extra.h"
-#include "modules/juce_opengl/juce_opengl.h"
+#include "MLJuceOpenGL.h"
+
 
 #endif // __JUCE_HEADER_H__
