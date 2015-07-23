82418b543ce4c0f260893ea8e1cca3b882a205be
Randy Jones <randy@madronalabs.com>
2015-07-22 23:25:25 -0700

    adding Hello Soundplane project

---
+++ Developer extras/HelloSoundplane/HelloSoundplane/HelloSoundplane.cpp
@@ -0,0 +1,9 @@
+//
+//  HelloSoundplane.cpp
+//  HelloSoundplane
+//
+//  Created by Randy Jones on 7/22/15.
+//  Copyright (c) 2015 Madrona Labs. All rights reserved.
+//
+
+#include "HelloSoundplane.h"
