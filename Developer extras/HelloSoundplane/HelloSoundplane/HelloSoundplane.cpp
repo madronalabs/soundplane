33533448d949fe2a982667d8ad609833e87fdf4a
Randy Jones <randy@madronalabs.com>
2015-07-23 00:02:03 -0700

    remove unused files

--- Developer extras/HelloSoundplane/HelloSoundplane/HelloSoundplane.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  HelloSoundplane.cpp
-//  HelloSoundplane
-//
-//  Created by Randy Jones on 7/22/15.
-//  Copyright (c) 2015 Madrona Labs. All rights reserved.
-//
-
-#include "HelloSoundplane.h"
